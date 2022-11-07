a590c2ea27f8525eaba6a811b4a8cb4792d2ffb1
sndb <sndb@sndb.xyz>
2022-11-07 10:58:47 +0300

    refactor binary search

--- c/search_insert_position.c
+++ c/search_insert_position.c
@@ -2,13 +2,13 @@ int searchInsert(int *nums, int numsSize, int target) {
 	int min = 0;
 	int max = numsSize - 1;
 	while (min <= max) {
-		int i = (min + max) / 2;
-		if (nums[i] == target)
-			return i;
-		if (nums[i] > target)
-			max = i - 1;
+		int mid = (min + max) / 2;
+		if (nums[mid] > target)
+			max = mid - 1;
+		else if (nums[mid] < target)
+			min = mid + 1;
 		else
-			min = i + 1;
+			return mid;
 	}
 	return min;
 }
