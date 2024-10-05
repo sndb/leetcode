61ea9a9945c84699ba886b7e9ab0427af2d8c7d4
sndb <ckyg@proton.me>
2024-10-06 00:46:13 +0300

    delete the middle node of a linked list in go

---
+++ go/delete_the_middle_node_of_a_linked_list.go
@@ -0,0 +1,22 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteMiddle(head *ListNode) *ListNode {
+	if head.Next == nil {
+		return nil
+	}
+
+	var prev *ListNode
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		prev = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	prev.Next = slow.Next
+	return head
+}
